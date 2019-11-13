901b79609e536910a5ce4d2e2efe04abe48b0df2
Tony3641 <tony3641@126.com>
2019-11-12 23:12:09 -0500

    add HW20

---
+++ HW20/src/ProgramWithIO.java
@@ -0,0 +1,114 @@
+import components.map.Map;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class ProgramWithIO {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private ProgramWithIO() {
+    }
+
+    /**
+     * Inputs a "menu" of words (items) and their prices from the given file and
+     * stores them in the given {@code Map}.
+     *
+     * @param fileName
+     *            the name of the input file
+     * @param priceMap
+     *            the word -> price map
+     * @replaces priceMap
+     * @requires <pre>
+     * [file named fileName exists but is not open, and has the
+     *  format of one "word" (unique in the file) and one price (in cents)
+     *  per line, with word and price separated by ','; the "word" may
+     *  contain whitespace but no ',']
+     * </pre>
+     * @ensures [priceMap contains word -> price mapping from file fileName]
+     */
+    private static void getPriceMap(String fileName,
+            Map<String, Integer> priceMap) {
+        SimpleReader in = new SimpleReader1L(fileName);
+        while (!in.atEOS()) {
+            String s = in.nextLine();
+            String word = "";
+            String price = "";
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ',') {
+                    word = s.substring(0, i);
+                    price = s.substring(i + 1);
+                }
+            }
+            int priceInt = Integer.parseInt(price);
+            priceMap.add(word, priceInt);
+        }
+    }
+
+    /**
+     * Input one pizza order and compute and return the total price.
+     *
+     * @param input
+     *            the input stream
+     * @param sizePriceMap
+     *            the size -> price map
+     * @param toppingPriceMap
+     *            the topping -> price map
+     * @return the total price (in cents)
+     * @updates input
+     * @requires <pre>
+     * input.is_open and
+     * [input.content begins with a pizza order consisting of a size
+     *  (something defined in sizePriceMap) on the first line, followed
+     *  by zero or more toppings (something defined in toppingPriceMap)
+     *  each on a separate line, followed by an empty line]
+     * </pre>
+     * @ensures <pre>
+     * input.is_open and
+     * #input.content = [one pizza order (as described
+     *              in the requires clause)] * input.content and
+     * getOneOrder = [total price (in cents) of that pizza order]
+     * </pre>
+     */
+    private static int getOneOrder(SimpleReader input,
+            Map<String, Integer> sizePriceMap,
+            Map<String, Integer> toppingPriceMap) {
+        int total = 0;
+
+        for (Map.Pair<String, Integer> item : sizePriceMap) {
+            total += item.value();
+        }
+        for (Map.Pair<String, Integer> item : toppingPriceMap) {
+            total += item.value();
+        }
+        return total;
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Put your main program code here
+         */
+        /*
+         * Close input and output streams
+         */
+        in.close();
+        out.close();
+    }
+
+}
